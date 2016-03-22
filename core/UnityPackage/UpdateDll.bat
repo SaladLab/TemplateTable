2d8f638df95df834fafc2c3f4c484abba219cad8
Esun Kim <veblush@gmail.com>
2016-03-22 14:15:31 +0900

    Update build script

--- core/UnityPackage/UpdateDll.bat
+++ core/UnityPackage/UpdateDll.bat
@@ -5,6 +5,7 @@ SET SRC_JSON=..\..\plugins\TemplateTable.Json.Net35\bin\Release
 SET SRC_PROTOBUF=..\..\plugins\TemplateTable.Protobuf.Net35\bin\Release
 SET DST=.\Assets\Middlewares\TemplateTable
 SET PDB2MDB=..\..\tools\unity3d\pdb2mdb.exe
+SET SFK=..\..\tools\sfk\sfk.exe
 
 %PDB2MDB% "%SRC%\TemplateTable.dll"
 %PDB2MDB% "%SRC_JSON%\TemplateTable.Json.dll"
@@ -15,6 +16,6 @@ COPY /Y "%SRC_JSON%\TemplateTable.Json.dll*" %DST%
 COPY /Y "%SRC_PROTOBUF%\TemplateTable.Protobuf.dll*" %DST%
 
 REM Remove public token of referenced Newtonsoft.Json from TemplateTable.Json.dll
-..\..\tools\sfk\sfk.exe rep %DST%\TemplateTable.Json.dll -pat -bin /0830ad4fe6b2a6aeed/000000000000000000/ -yes
+%SFK% rep %DST%\TemplateTable.Json.dll -pat -bin /0830ad4fe6b2a6aeed/000000000000000000/ -yes
 
 popd
